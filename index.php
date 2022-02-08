f6d04e8b2a9df0f27ff1668d6eefa5b4eae50d46
PSngsn <98947698+PSngsn@users.noreply.github.com>
2022-02-08 13:56:10 +0800

    Update index.php

--- index.php
+++ index.php
@@ -12,18 +12,18 @@
 <table border="4" align="center" bordercolor="white">
 
 <tr>
-        <p><br><br><br><td length="10%" width="10%"><font color="white" size="27"><center><i> "MATH" <br> link </i></center></font></td>
+        <p><br><br><br><td length="10%" width="10%"><font color="black" size="27"><center><i> "MATH" <br> link </i></center></font></td>
 
 
-        <br><td length="10%" width="10%"><font color="white" size="27"><center><i> "CAE" <br> link </i></center></font></td>
+        <br><td length="10%" width="10%"><font color="black" size="27"><center><i> "CAE" <br> link </i></center></font></td>
 
 
-        <br><td length="10%" width="10%"><font color="white" size="27"><center> <i><i> "CLE" <br> link </i></center></font></td>
+        <br><td length="10%" width="10%"><font color="black" size="27"><center> <i><i> "CLE" <br> link </i></center></font></td>
 </tr>
 
 </table>
 
-<p><br><br><h3><font face="Arial" color="white"><center><b><i> "9D MEMBERS OF GROUP 21" </i></b></center></h3> <h4><center><b> Echel Snook <br>
+<p><br><br><h3><font face="Arial" color="black"><center><b><i> "9D MEMBERS OF GROUP 21" </i></b></center></h3> <h4><center><b> Echel Snook <br>
 Cloud Kent <br> Giahn Buenaflor <br>Pauliana Singson <br> Thea Gayacao <br></b></center></h4></font> <br><br>
 
 
